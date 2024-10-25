9e46c254e5c4ccec1ee836765c75f3a52c62fb2d
NazimAkhter <nazim.akhter99@gmail.com>
2024-10-25 17:49:40 +0500

    Second Commit

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,11 +1,9 @@
 export default function Footer() {
   return (
-    <div className="flex flex-col justify-center">
-      <div className="bg-blue-900 h-24 mt-40">
+        <div className=" flex flex-col justify-center bg-blue-900 h-24 mt-8">
         <h1 className="text-white text-xl text-center mt-2  ">Class-05-Home Work</h1>
         <h1 className="text-white text-lg text-center">Nazim Akhter</h1>
         <h1 className="text-white text-lg text-center">GIAIC - Roll No. 406722</h1>
-      </div>
-      </div>   
+      </div> 
   );
 }
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
+import logo from "@/../../image/logo.jpeg"
 
 function Navbar() {
   return (
-    <div className="bg-blue-900 flex h-20 justify-between items-center">
+    <div className="bg-blue-900 flex h-20 justify-between items-center sticky top-0">
     
       <div className=" text-white text-xl flex h-16 justify-start items-center">
-        <Image src={require("../../../image/logo.jpeg")} alt="Company Logo" width={200} height={200} className="ml-9 w-16 hover:w-24 hover:transition-all hover:cursor-pointer rounded-full"/>
+        <Image src={logo} alt="Company Logo" width={200} height={200} className="ml-9 w-16 hover:w-24 hover:transition-all hover:cursor-pointer rounded-full"/>
         <h1 className="text-white font-sans font-semibold text-3xl ml-7 hover:shadow-slate-100">Z.H Graphics & Printers</h1>  
       </div>
       
